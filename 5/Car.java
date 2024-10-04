2b6405fa2ab9adb46974b9f20031070008f903a5
Khankeldi Shakhmukhanbetov <khanekshakh@gmail.com>
2024-10-04 21:14:14 +0500

    Добавил папку 5 который буду сохранить работы дз5

---
+++ 5/Car.java
@@ -0,0 +1,10 @@
+public class Car implements IVehicle {
+    private String marka;
+    private String model;
+    private String topliva;
+
+    public Car(String marka, String model, String topliva) {
+        this.make = marka;
+        this.model = model;
+        this.topliva = topliva;
+    }
